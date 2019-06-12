46f6b26da27d5cc5ce547f93168b66e3b5be279e
fabiengoyhex <fabien.goyhex@gmail.com>
2019-06-12 09:00:05 +0200

    added dataBase

---
+++ migrations/20190608105220-create-user.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('Users', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			email: {
+				allowNull: false,
+				type: Sequelize.STRING
+			},
+			username: {
+				type: Sequelize.STRING
+			},
+			password: {
+				type: Sequelize.STRING
+			},
+			bio: {
+				allowNull: true,
+				type: Sequelize.STRING
+			},
+			isAdmin: {
+				type: Sequelize.BOOLEAN
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('Users');
+	}
+};
